10538e48dca0ab1b29abe2edb8a1c0f6028bec6f
Victor <victorbabich@mail.ru>
2017-08-02 11:46:02 +0500

    up

--- game.js
+++ game.js
@@ -34,40 +34,9 @@ field.forEach(function(cell, idx) {
 
 function Player(name) {
 	this.name = name;
-	this.start = [0, 0];
+	this.active = false;
+	this.start = 0;
 };
-
-var game = {
-	makeMove: function() {
-		game.player.position += game.throwDice();
-		ctx.fillStyle = 'black';
-		ctx.fillRect(field[game.player.position].x, field[game.player.position].y, side, side);
-		console.log(game.player.position)
-	},
-	throwDice: function() {
-		let rand = 0.5 + Math.random() * 6;
-    rand = Math.round(rand);
-    return rand;
-	},
-	players: [
-		{
-			player: {
-				name: "name1",
-				active: false,
-				position: 0
-			}
-		}, 
-		{
-			player: {
-				name: "name2",
-				active: false,
-				position: 0
-			}
-		}
-	]
-}
-
-
 
 var dice = document.getElementById('dice');
 dice.onclick = function(event) {
@@ -129,16 +98,54 @@ function makeTeleports() {
 makeTeleports();
 
 
-teleports.forEach(function(teleport) {
-	ctx.fillStyle = 'green';
-	ctx.beginPath();
-	ctx.moveTo(field[teleport.start].x + side/2, field[teleport.start].y + side/2);
-	ctx.lineTo(field[teleport.end].x + side/2, field[teleport.end].y + side/2);
-	ctx.closePath();
-	ctx.strokeStyle = 'rgb(0,128,0)';
-	ctx.lineWidth = 5;
-	ctx.stroke();
+teleports.forEach(function(teleport, idx) {
+	if(idx < teleports.length/2) { 
+		ctx.strokeStyle = 'rgb(0,128,0)';
+	}	else {
+		ctx.strokeStyle = 'rgb(256,0,0)';
+	}
+		ctx.beginPath();
+		ctx.moveTo(field[teleport.start].x + side/2, field[teleport.start].y + side/2);
+		ctx.lineTo(field[teleport.end].x + side/2, field[teleport.end].y + side/2);
+		ctx.closePath();
+		ctx.lineWidth = 5;
+		ctx.stroke();
 })
 
 
+var game = {
+	makeMove: function() {
+		game.player.position += game.throwDice();
+		if(game.player.position >= amount * amount - 1) {
+			ctx.fillStyle = 'red';
+			ctx.font = "italic " + 32 + "pt Arial";
+			ctx.fillText("Ты победил, сука!", 30, amount*side/2);
+		}
+		teleports.forEach(function(teleport, idx) {
+			if(idx < teleports.length/2 && game.player.position == teleport.start) { 
+				game.player.position = teleport.end;
+				console.log("лестница")
+			}	else if(idx >= teleports.length/2 && game.player.position == teleport.end) {
+				game.player.position = teleport.start;
+				console.log("змея")
+			}
+		});
+		ctx.fillStyle = 'black';
+		ctx.fillRect(field[game.player.position].x, field[game.player.position].y, side, side);
+		console.log(game.player.position)
+	},
+	throwDice: function() {
+		let rand = 0.5 + Math.random() * 6;
+    rand = Math.round(rand);
+    return rand;
+	},
+	player: { 
+		name: "name1",
+		active: false,
+		position: 0
+	}
+}
+
 console.log(teleports)
+
+
